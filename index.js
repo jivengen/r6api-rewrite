7c9afa359746c036a753c6ff62864592dc908f40
Lars Brix <lars.brix@vispiron.de>
2017-02-21 16:30:38 +0100

    add more commands (NO TESTS YET)

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const auth = require("./lib/auth");
 const errors = require("./lib/errors")
 const findByName = require("./lib/commands/findByName");
+const getCurrentName = require("./lib/commands/getCurrentName");
+const getLevel = require("./lib/commands/getLevel");
+const getPlaytime = require("./lib/commands/getPlaytime");
+const getRank = require("./lib/commands/getRank");
+const getStats = require("./lib/commands/getStats");
 
 module.exports.auth = auth;
 module.exports.errors = errors;
 module.exports.api = {
-    findByName
+    findByName,
+    getCurrentName,
+    getLevel,
+    getPlaytime,
+    getRank,
+    getStats
 }
--- temp.js
+++ temp.js
@@ -1,12 +1,15 @@
 const r6api = require("./index");
 const fixture = require("./test.login");
+const debug = require("debug")("temp");
+const log = res => { debug("%O", res); return res; }
 
 r6api.auth.login(fixture.email, fixture.password)   
-    .then(() => r6api.api.findByName(["NaCleptic"]))
+    .then(() => r6api.api.findByName(["LaxisB"]))
+    .then(res => r6api.api.getRank(res[0].id), 5)
     .then(res => {
-        console.log(res);
-        process.exit(0);
+        console.log(JSON.stringify(res, null, 2));
     })
+    .then(() => process.exit(0))
     .catch(err => {
         console.error(err);
         process.exit(1);
