345e746b62efe85dd0b05900b90833f22538bb0b
Lars Brix <lars.brix@vispiron.de>
2017-02-21 10:50:59 +0100

    add findByName

---
+++ __test__/findByName.test.js
@@ -0,0 +1,28 @@
+const login = require("../test.login");
+const auth = require("../lib/auth");
+const findByName = require("../lib/commands/findByName"); 
+
+const acc = {
+    LaxisSmurf: { alias: "NaCleptic", id: "532dae03-7c7f-42a1-89d4-f5bbd8f67573" },
+    Muppet: { alias: "NaughtyMuppet", id: "ccd28cc4-845e-4726-8cf8-e2cac4de82a2"}
+};
+
+
+
+describe("test", function () {
+    it("can find a single account", () => {
+        auth.login(login.email, login.password)
+            .then(() => findByName(acc.LaxisSmurf.alias))
+            .then(res => {
+                expect(res.length).toBe(1);
+                expect(res[0].id).toBe(acc.LaxisSmurf.id);
+            });
+    });
+    it("can find multiple accounts", () => {
+        auth.login(login.email, login.password)
+            .then(() => findByName([acc.LaxisSmurf.alias, acc.Muppet.alias]))
+            .then(res => {
+                expect(res.length).toBe(2);
+            });
+    });
+});    
--- temp.js
+++ temp.js
@@ -1,6 +1,11 @@
 const auth = require("./lib/auth");
 const fixture = require("./test.login");
+const findByName = require("./lib/commands/findByName")
 
 auth.login(fixture.email, fixture.password)
-    .then(console.log)
+    .then(() => findByName(["NaCleptic"]))
+    .then(res => {
+        console.log(res);
+        process.exit(0);
+    })
     .catch(console.error);
