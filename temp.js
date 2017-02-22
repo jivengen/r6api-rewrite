82adeb4109666bcc819b850ff557e9636212ff5a
Lars Brix <lars.brix@vispiron.de>
2017-02-22 11:28:25 +0100

    add getStats

--- temp.js
+++ temp.js
@@ -5,7 +5,7 @@ const log = res => { debug("%O", res); return res; }
 
 r6api.auth.login(fixture.email, fixture.password)   
     .then(() => r6api.api.findByName(["LaxisB"]))
-    .then(res => r6api.api.getRank(res[0].id), 5)
+    .then(res => r6api.api.getStats(res[0].id))
     .then(res => {
         console.log(JSON.stringify(res, null, 2));
     })
